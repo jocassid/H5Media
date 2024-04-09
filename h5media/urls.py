e7a358f45787355e116983eb58634d4a13d3357c
John Cassidy <jocassid@gmail.com>
2024-04-08 22:28:53 -0400

    Added logout page

--- h5media/urls.py
+++ h5media/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, PasswordResetView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+)
 from django.urls import path
 
 from h5media.views import HomeView
@@ -26,6 +30,11 @@ urlpatterns = [
         'login/',
         LoginView.as_view(template_name='login.html'),
         name='login',
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        name='logout',
     ),
     path(
         'password-reset/',
